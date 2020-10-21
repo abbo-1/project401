5864b00c3021d106456f3e19c58d59ceb196ee01
abbo-1 <abbott.kevinj@gmail.com>
2020-10-21 17:57:48 -0500

    trying to move props

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const [showMessage, setMessageOff] = useState(false);
 const store = createStore(reducer)
 
 function thin() {
-  console.log("check this out " + props.displayPassengers)
+  console.log(JSON.stringify(props.displayPassengers))
 }
 
   return (
@@ -46,7 +46,7 @@ function thin() {
 
          {/* <button onClick={props.displayPassengers}>Lifeboat #1</button> */}
          <button class="lifeboatBtn" onClick={thin()}>Lifeboat #1</button>
-         <button class="lifeboatBtn" onClick={() => {console.log("look at this" + props)}}>Lifeboat #2</button>
+         <button class="lifeboatBtn" onClick={() => {console.log(JSON.stringify(props.displayPassengers))}}>Lifeboat #2</button>
          <div>Surviviors of lifeboat #1 Include: {props.lifeboat1Passenger}</div>
 
       {/* </header> */} 
