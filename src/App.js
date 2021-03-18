bea21c7dce4fd113f63891e8dfb7a65286ff8c93
abbo-1 <abbott.kevinj@gmail.com>
2021-03-18 18:03:43 -0500

    deleted unneeded code

--- src/App.js
+++ src/App.js
@@ -32,13 +32,16 @@ const initialState = {
       {/* <header className="App-header"> */}
       {/* {message} */}
       {showMessage ? (<div id='welcomeMessage'>
-        In the early hours of April 15th 1912, The RMS Titanic sank beneath the waves. Beginning at 11:45 PM and until the ship's last moments, a courageous few worked tirelessly to launch lifeboats. 
+        In the early hours of April 15th 1912, The RMS Titanic sank beneath the waves. From the time of collision at 11:45 PM, until the ship's last moments, a courageous few worked tirelessly to launch lifeboats. 
         <br/><br/>  
         By the end of the night only 712 people, less then 1/3rd of those who were on board, survived. 
         <br/> <br/> 
         Click on a lifeboat to learn more about each boat's survivors.
         <br/>  
-        <button onClick={() => setMessageOff(null)} id="exploreBtn">Explore</button>
+        <button onClick={() => setMessageOff(null)} 
+        id="exploreBtn">
+        Explore
+        </button>
 
         </div>) : true}
 
@@ -48,25 +51,6 @@ const initialState = {
          {/* <button onClick={props.displayPassengers}>Lifeboat #1</button> */}
          {/* <button class="lifeboatBtn" onClick={thin()}>Lifeboat #1</button> */}
 
-         <button class="lifeboatBtn" onClick={() => {console.log(JSON.stringify(props.displayPassengers))}}>Lifeboat #2</button>
-
-         <div>Surviviors of lifeboat #1 Include: {props.lifeboat1Passenger}</div>
-                  <div>Surviviors of lifeboat #1 Include: {props.lifeboat1Passenger}</div>
-                  <div>Me: {props.passengers}</div>
-                  <div>Ka: {props.lifeboat1Passenger}</div>
-                  <div>Ka: {props.lifeboat1Passenger}</div>
-
-\
-         <br/>
-
-
-         <button onClick={decrement}>Minus - View Lifeboat 4 Passengers</button>
-          <span>Click to learn more Person 1{props.counter}</span>
-          <span>Click to learn more Person 44{props.counter}</span>
-          <p className="counter">Counter: {initialState.counter} </p>
-          {/* <p className="counter">Countermes Watson: {getState().counter} </p> */}
-         <button onClick={increment}>Add + - Remove Lifeboat 4 Passenger</button>
-         
       {/* </header> */} 
       </ScrollContainer>
     </div>
@@ -74,3 +58,4 @@ const initialState = {
 }
 
 export default connect()(App);
+
