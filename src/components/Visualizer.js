517f8fbd2ca7e1b234b87a7e82c1779058fc8b37
abbo-1 <abbott.kevinj@gmail.com>
2021-06-17 17:56:03 -0500

    passenger visualizer works on a basic level

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -13,17 +13,49 @@ const Visualizer = () => {
     //     <FaceIcon />
     // }
 
+    // const PeopleCounter = ({n}) => {
+    //     let people = []
+    //     for (let i = 0; i < n; ++i) {
+    //         people.push(<FaceIcon />)
+    //     }
+    // }
+
+
+    const displayIt = () => {
+        setShowPeople(true);;
+    }
+
+    // const n = 1492
+    // const tryThis = [...Array(n)].map((e, i) => <span key={i}>test</span>)
+
+
+
+    // const personIcon = () => {
+    // [...Array(1493)].map((elementInArray, i) => ( 
+    // <div key={i}> Whatever needs to be rendered repeatedly </div> 
+    // ) 
+
+    // )}
+
+
+
+
+ 
+
 
     return (
         <div>
 
-        <div
-          show={showPeople} 
-          onHide={handleClosePeople}>
-        <FaceIcon />
+        <div>
+            { [...Array(1493)].map((v, i) => <FaceIcon /> ) }
+
+            <FaceIcon style={{color: "green"}} />
+            <FaceIcon style={{color: "purple"}} />
+            <FaceIcon style={{color: "yellow"}}/>
+            {showPeople ? <FaceIcon /> : null}
         </div>
 
-        <Button onClick={handleShowPeople}>test</Button>
+        <Button onClick={displayIt}>turtles</Button>
         </div>
     )
 };
