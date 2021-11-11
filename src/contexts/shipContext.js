3ef2c9d80a0bd015ff104a16fc23933664dc9eb8
abbo-1 <abbott.kevinj@gmail.com>
2021-11-11 17:44:51 -0600

    switch case

--- src/contexts/shipContext.js
+++ src/contexts/shipContext.js
@@ -3,34 +3,35 @@ import titanic from '../images/titanic2.png'
 import titanic1140 from '../images/titanic1140.png'
 import HUD from '../components/HUD'
 
-function ShipContext(time) {
-        switch (time) {
+function ShipContext(scale) {
+    {(() => {
+        switch (scale) {
             case "elevenForty": 
                 return (
-                    map= <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
-                    time= "11:40 pm",
-                    date= "April 14th, 1912"
+                    map = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
+                    time = "11:40 pm",
+                    date = "April 14th, 1912"
                 )                
-        
             case "elevenFifty":
                 return (
-                    map= <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
-                    time= "11:50 pm",
-                    date= "April 14th, 1912"                
+                    map = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
+                    time = "11:50 pm",
+                    date = "April 14th, 1912"                
                 )
             case "elevenFiftyTwo":
                 return (
-                    map= <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
-                    time= "12:10 am",
-                    date= "April 15th, 1912"                
+                    map = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/>,
+                    time = "12:10 am",
+                    date = "April 15th, 1912"                
                 )
             default: 
                 return (
-                    map= <img src={titanic} id="shipLayout" alt="ship blueprint"/>,
-                    time= "",
-                    date= ""             
+                    map = <img src={titanic} id="shipLayout" alt="ship blueprint"/>,
+                    time = "",
+                    date = ""           
                 )
             }
+        })()}
 
     return (
         <>
@@ -40,35 +41,3 @@ function ShipContext(time) {
 }
 
 export default ShipContext
-
-
-
-
-//OLD STORE
-// import React, { createContext, useContext, useReducer } from 'react';
-
-// const StoreContext = createContext();
-// const initialState = {layout = <img src={titanic} id="shipLayout" alt="ship blueprint"/> }
-
-// const reducer = (state, action) => {
-//     switch(action.type) {
-//         case "elevenForty":
-//             return {
-//                 map: {layout = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/> },
-//                 time: "11:40 pm",
-//                 date: "April 14th, 1912"                
-//             }
-//         case "elevenFifty":
-//             return {
-//                  map: {layout = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/> },
-//                 time: "11:50 pm",
-//                 date: "April 14th, 1912"                
-//             }
-//         case "midnight":
-//             return {
-//                 map: {layout = <img src={titanic1140} id="shipLayout" alt="ship blueprint"/> },
-//                 time: "12:00 am",
-//                 date: "April 15th, 1912"                
-//             }
-//     }
-// }
